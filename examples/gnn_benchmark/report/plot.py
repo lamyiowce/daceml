e823a2d1a524e3829993c8d41cb86672a7890f78
Julia Bazinska <julia@bazinski.com>
2023-05-04 15:18:46 +0200

    WIP roofline plot

--- examples/gnn_benchmark/report/plot.py
+++ examples/gnn_benchmark/report/plot.py
@@ -1,12 +1,20 @@
 import re
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
 DATA_FOLDER = Path(__file__).parent / 'data'
 PLOT_FOLDER = Path(__file__).parent / 'plots'
 
+DEFAULT_LABEL_MAP = {
+    'torch_csr': 'Torch CSR',
+    'torch_edge_list': 'Torch Edge List',
+    'dace_csr': 'Dace CSR',
+    'dace_coo': 'Dace COO',
+    'dace_csc': 'Dace CSC',
+}
 
 def get_colors(names: pd.Series):
     reds = ['indianred', 'lightcoral', 'darkred', 'red']
@@ -85,21 +93,14 @@ def make_plot(full_df, name, label_map=None, bwd_df=None):
     plt.show()
 
 
-def make_performance_plot(full_df, name):
-    df = full_df.pivot(index='Size', columns='Name', values='Mean')
-    std_df = full_df.pivot(index='Size', columns='Name', values='Stdev')
-    flop_base = 13264 * 2 + 2 * 2704 * 1433
-    print(df)
-    flop = df.index * flop_base / (1000 * 1000 * 1000 * 1000)  # tflops
-    # df['FLOP'] = flop
-    print(df)
-    df_flops = pd.DataFrame()
-    print(df_flops)
-    for col in df.columns:
-        df_flops[col] = flop / (df[col] / 1000)
-    print(df_flops)
+def make_performance_plot(full_df, name, labels=None, title=None):
+    assert 'single' in name
+    df, df_flops, _ = get_flops_df(full_df, name)
 
-    colors = ['mediumseagreen', 'indianred', 'lightcoral']
+    colors = get_colors(df.columns)
+    default_labels = DEFAULT_LABEL_MAP.copy()
+    default_labels.update(labels or {})
+    labels = default_labels
 
     ax = df_flops.plot(figsize=(5, 5), kind='barh', ylabel='Runtime [ms]',
                        xlabel='Hidden size', color=colors)
@@ -107,15 +108,17 @@ def make_performance_plot(full_df, name):
     ax.yaxis.grid(color='lightgray', linestyle='--')
     ax.set_xlabel("Performance [TFLOP / s]")
     ax.set_ylabel("Hidden size")
-    peak_v100 = 15.7  # Page 5: https://images.nvidia.com/content/volta-architecture/pdf/volta-architecture-whitepaper.pdf
+
+    labels = [labels.get(name, name) for name in df.columns]
+    plt.legend(labels, loc='lower center')
+    peak_v100 = 14  # https://images.nvidia.com/content/technologies/volta/pdf/tesla-volta-v100-datasheet-letter-fnl-web.pdf
     ax.plot([peak_v100, peak_v100], [-10, 10], color='black', linestyle='--',
             linewidth=1)
-    ax.text(peak_v100 * 0.95, 1, f'V100 peak: {peak_v100}', rotation=90,
+    ax.text(peak_v100 * 0.95, 1, f'V100 peak: {peak_v100} TFLOP / s', rotation=90,
             verticalalignment='center',
             horizontalalignment='left', fontsize=12)
     ax.set_xlim(xmax=peak_v100 * 1.1)
-    plt.title(name.upper())
-    # plt.legend(labels, loc='upper left' if name == 'gcn' else 'lower right')
+    plt.title(title or name.upper())
     plt.xticks(rotation=0)
 
     for container in ax.containers:
@@ -123,8 +126,83 @@ def make_performance_plot(full_df, name):
             ax.bar_label(container, fmt="%.2f")
 
     plt.tight_layout()
-    plt.savefig(PLOT_FOLDER / f'{name}-performance.pdf', bbox_inches='tight')
+    plt.savefig(PLOT_FOLDER / f'{pd.Timestamp.today().strftime("%m-%d")} {name}-performance.pdf', bbox_inches='tight')
     plt.show()
+
+
+def get_flops_df(full_df, name):
+    df = full_df.pivot(index='Size', columns='Name', values='Mean')
+    if name == 'gcn-single-ogbn-arxiv':
+        num_nodes = 169343
+        num_edges = 1166243
+        num_features = 128
+    elif name == 'gcn-single-cora':
+        num_nodes = 2708
+        num_edges = 10556
+        num_features = 1433
+    else:
+        raise ValueError(f'Unknown name: {name}')
+    # This just need to be multiplied by hidden_size:
+    # flop = 2 * num_nodes * num_features * hidden_size + 2 * num_edges * hidden_size + num_nodes * hidden_size
+    flop_base = 2 * num_nodes * num_features + 2 * num_edges + num_nodes
+    print(df)
+    flop = df.index * flop_base / (1000 * 1000 * 1000 * 1000)  # tflops
+    print(df)
+    df_flops = pd.DataFrame()
+    print(df_flops)
+    for col in df.columns:
+        df_flops[col] = flop / (df[col] / 1000)
+    print(df_flops)
+
+
+    mem = 4 * (4*num_nodes * df.index + num_nodes * num_nodes + 4 * df.index * num_edges + df.index) / 1000 / 1000 / 1000
+    op_int = np.zeros((len(df.index), len(df.columns)))
+    for i, col in enumerate(df_flops.columns):
+        op_int[:, i] = df_flops[col] * 1000 / mem # FLOPS / B
+
+    df_op_int = pd.DataFrame(op_int, index=df.index, columns=df.columns)
+    print(op_int)
+    print("FLOPS", df_flops)
+    print("Op INT", df_op_int)
+    return df, df_flops, df_op_int
+
+
+def make_roofline_plot(df, name):
+    """Make a roofline plot for V100-PCIE-16GB."""
+    _, df_flops, df_op_int = get_flops_df(df, name)
+    df_flops = df_flops * 1000 # GFLOPS
+    ax = plt.figure(figsize=(8, 5))
+    # Set both axes to logarithmic scaling.
+    plt.xscale('log')
+    plt.yscale('log')
+
+    # Set axis limits.
+    # plt.xlim(1e-1, 1e2)
+    plt.ylim(1e-1, 1e5)
+
+    # Set axis labels.
+    plt.xlabel('Operational Intensity [FLOP/Byte]')
+    plt.ylabel('Performance [GFLOP/s]')
+
+    # Plot the theoretical performance peak for V100.
+    peak_v100 = 14 * 1000  # https://images.nvidia.com/content/technologies/volta/pdf/tesla-volta-v100-datasheet-letter-fnl-web.pdf
+    plt.plot([1e-1, 1e2], [peak_v100, peak_v100], color='black')
+    plt.text(1, peak_v100 * 1.1, f'Theoretical FP32: {peak_v100/1000} TFLOP/s', rotation=0,
+            verticalalignment='bottom',
+            horizontalalignment='left', fontsize=12)
+
+    # Plot the memory bound.
+    mem_bound = 900 # gb / s
+    plt.plot([1/mem_bound, peak_v100 / mem_bound], [1, peak_v100], color='black')
+    plt.text(1, mem_bound * 1.1, f'HBM bandwidth: {mem_bound} GB/s', rotation=32, rotation_mode='anchor',
+             fontsize=12)
+
+    for col in df_flops.columns:
+        plt.plot(df_op_int[col], df_flops[col], label=col)
+
+    plt.legend()
+    plt.show()
+
 
 
 def read_many_dfs(filenames, name_to_replace=None, backward: bool = True,
@@ -162,6 +240,30 @@ def main():
     # plot_backward("data/24-04-gcn-reduce-gpuauto-simplify", model='GCN')
     # plot_backward("data/24-04-gcn-single-reduce-gpuauto-simplify", model='GCN Single layer')
 
+    cora_single_df, _ = read_many_dfs(
+        filenames=['04.05.10.07-gcn_single_layer-cora-186177.csv',]
+    )
+    make_performance_plot(full_df=cora_single_df, name='gcn-single-cora', title="Single GCN layer performance, Cora")
+
+    arxiv_single_df, _ = read_many_dfs(
+        filenames=['04.05.10.25-gcn_single_layer-ogbn-arxiv-186177.csv',]
+    )
+    make_performance_plot(full_df=arxiv_single_df, name='gcn-single-ogbn-arxiv', title="Single GCN layer performance, OGB Arxiv")
+
+    make_roofline_plot(cora_single_df, name='gcn-single-cora',)
+    make_roofline_plot(arxiv_single_df, name='gcn-single-ogbn-arxiv',)
+    # plot_midthesis_main_datasets()
+    # plot_midthesis_additional_datasets()
+    # plot_stream_comparison()
+
+
+def plot_midthesis_additional_datasets():
+    plot_backward('03.05.09.52-gcn-pubmed-185244', plot_title='GCN, Pubmed')
+    plot_backward('03.05.10.26-gcn-flickr-185244', plot_title='GCN, Flickr')
+    plot_backward('03.05.10.09-gcn-citeseer-185244', plot_title='GCN, Citeseer')
+
+
+def plot_midthesis_main_datasets():
     arxiv_df, arxiv_bwd_df = read_many_dfs(
         filenames=['01.05.11.34-gcn-ogbn-arxiv-183528.csv',
                    '01.05.12.33-gcn-ogbn-arxiv-183567.csv',
@@ -169,21 +271,15 @@ def main():
                    '03.05.18.24-gcn-ogbn-arxiv-alt-sizes-185598.csv',
                    '03.05.19.41-gcn-ogbn-arxiv-alt-sizes-185634.csv']
     )
-    plot_backward(tag='arxiv', sizes=[16, 64, 256, 1024], plot_title='GCN, OGBN Arxiv', df=arxiv_df, bwd_df=arxiv_bwd_df)
+    plot_backward(tag='arxiv', sizes=[16, 64, 256, 1024], plot_title='GCN, OGBN Arxiv', df=arxiv_df,
+                  bwd_df=arxiv_bwd_df)
     plot_backward(tag='arxiv', plot_title='GCN, OGBN Arxiv', df=arxiv_df, bwd_df=arxiv_bwd_df)
-
     cora_df, cora_bwd_df = read_many_dfs(
         filenames=['01.05.11.17-gcn-cora-183528.csv',
                    '03.05.17.14-gcn-cora-csc-185561.csv',
                    '03.05.18.04-gcn-cora-alt-sizes-185598.csv'])
     plot_backward(tag="cora", sizes=[16, 64, 256, 1024], plot_title='GCN, Cora', df=cora_df, bwd_df=cora_bwd_df)
     plot_backward(tag="cora", plot_title='GCN, Cora', df=cora_df, bwd_df=cora_bwd_df)
-
-    # plot_backward('03.05.09.52-gcn-pubmed-185244', plot_title='GCN, Pubmed')
-    # plot_backward('03.05.10.26-gcn-flickr-185244', plot_title='GCN, Flickr')
-    # plot_backward('03.05.10.09-gcn-citeseer-185244', plot_title='GCN, Citeseer')
-    # plot_stream_comparison()
-
 
 
 def plot_stream_comparison():
@@ -208,14 +304,8 @@ def plot_backward(tag, plot_title, labels=None, df=None, bwd_df=None, sizes=None
         df = pd.read_csv(DATA_FOLDER / (tag + '.csv'), comment='#')
     if sizes is not None:
         df = df[df['Size'].isin(sizes)]
-    default_labels = {
-        'dace_autoopt_persistent_mem_csr': 'DaCe CSR',
-        'dace_csr': 'DaCe CSR',
-        'dace_coo': 'DaCe COO',
-        'dace_csc': 'DaCe CSC',
-        'torch_csr': 'PyG CSR',
-        'torch_edge_list': 'PyG edge list',
-    }
+
+    default_labels = DEFAULT_LABEL_MAP.copy()
     default_labels.update(labels or {})
     labels = default_labels
     make_plot(df, f"{plot_title}: forward pass", labels)
