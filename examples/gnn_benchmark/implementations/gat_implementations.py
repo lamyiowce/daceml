3099e0f3766a34f7831521580aa534dc847ef42c
jbazinska <julia.bazinska@gmail.com>
2023-05-08 16:41:45 +0200

    GAT cleanup

--- examples/gnn_benchmark/implementations/gat_implementations.py
+++ examples/gnn_benchmark/implementations/gat_implementations.py
@@ -10,6 +10,7 @@ from daceml.onnx.op_implementations.utils import op_implementation
 from daceml.onnx.op_implementations.utils import program_for_node
 from daceml.util.utils import in_desc_with_name
 from examples.gnn_benchmark import sparse
+from examples.gnn_benchmark.csrmm_libnode import csrmm
 from examples.gnn_benchmark.implementations.common import SparseLayerBase
 
 
@@ -41,95 +42,10 @@ class GATConvBase(SparseLayerBase, metaclass=abc.ABCMeta):
         return program_for_node(op, sdfg, state, node)
 
 
-@op_implementation(op="torch_geometric.nn.conv.gat_conv.GATConv",
-                   name="semester_thesis")
-class GATConvSemesterThesis(GATConvBase):
-    graph_format = sparse.CsrGraph
-    input_spec = {
-        "node_features": dace.float32,
-        "rowptrs": dace.int64,
-        "columns": dace.int64,
-    }
-
-    @staticmethod
-    def make_op(N: int, heads: int, num_out_features: int, num_entries: int,
-                dtype: dace.dtypes.Typeclasses, negative_slope: float,
-                do_bias: bool):
-        def gat_op(node_features, rowptrs, columns, lin_srcDOTweight,
-                   att_src, att_dst, output):
-            """
-            node_features: input features, N x F
-            rowptrs: rowptr, N+1
-            columns: col, num_entries
-            lin_srcDOTweight: H * F' x F
-            att_src: H x F
-            att_dst: H x F
-            output: N x H * F'
-            """
-
-            # Transform input features.
-            features = dace.define_local((N, heads, num_out_features),
-                                         dtype=dtype)
-            features_tmp = np.einsum('ij,kj->ik', node_features,
-                                     lin_srcDOTweight)
-            features[:] = np.reshape(features_tmp,
-                                     (N, heads, num_out_features))
-            # Compute node attention coefficients.
-            alpha_src = np.sum(features * att_src, axis=-1)  # shape: N x H
-            alpha_dst = np.sum(features * att_dst, axis=-1)  # N x H
-
-            # Calculate attention weights.
-            e = np.zeros((num_entries, heads), dtype=dtype)
-            softmax_sum = np.zeros((N, heads), dtype=dtype)
-
-            # TODO: Below loop can be flipped.
-            for l in dace.map[0:N]:
-                for v in dace.map[rowptrs[l]:rowptrs[l + 1]]:
-                    # Calculating e_l->colv
-                    colv = columns[v]
-                    e_tmp = alpha_src[l] + alpha_dst[colv]
-                    # LeakyReLU
-                    e_tmp = np.maximum(negative_slope * e_tmp, e_tmp)
-                    e_tmp = np.exp(e_tmp)
-                    e[v] = e_tmp
-                    softmax_sum[colv] += e_tmp
-
-            # Softmax normalization.
-            for j in dace.map[0:num_entries]:
-                colj = columns[j]
-                e[j] = e[j] / softmax_sum[colj]
-
-            # Implementation with loop flattening.
-            helper_row = dace.define_local((num_entries,), dtype=dace.int64)
-            for l in dace.map[0:N]:
-                for v in dace.map[rowptrs[l]:rowptrs[l + 1]]:
-                    helper_row[v] = l
-
-            output[:] = 0
-            for i in dace.map[0:num_entries]:
-                colv = columns[i]
-                b = helper_row[i]
-                if heads == 1:
-                    output[colv] += e[i] * features[b]
-                else:
-                    output[colv] += np.reshape(
-                        np.reshape(e[i], (heads, 1)) * features[b],
-                        (heads * num_out_features,))
-
-        if do_bias:
-            def bias_prog(node_features, rowptrs, columns, lin_srcDOTweight,
-                          att_src, att_dst, bias, output):
-                gat_op(node_features, rowptrs, columns, lin_srcDOTweight,
-                       att_src, att_dst, output)
-                for i, j in dace.map[0:N, 0:num_out_features * heads]:
-                    output[i, j] += bias[j]
-
-            return bias_prog
-        return gat_op
 
 
 @op_implementation(op="torch_geometric.nn.conv.gat_conv.GATConv",
-                   name="csr")
+                   name="csr_semester_thesis")
 class GATConvCSR(GATConvBase):
     graph_format = sparse.CsrGraph
     input_spec = {
@@ -159,9 +75,12 @@ class GATConvCSR(GATConvBase):
                                          dtype=dtype)
             features_tmp = np.einsum('ij,kj->ik', node_features,
                                      lin_srcDOTweight)
+
+            # features: N x H x F'
             features[:] = np.reshape(features_tmp,
                                      (N, heads, num_out_features))
             # Compute node attention coefficients.
+            # features * att_src: N x H x F
             alpha_src = np.sum(features * att_src, axis=-1)  # shape: N x H
             alpha_dst = np.sum(features * att_dst, axis=-1)  # N x H
 
