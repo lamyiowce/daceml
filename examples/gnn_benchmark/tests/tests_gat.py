6b5b34cc2fcf3bf2453969130b09c43d4bac0370
Julia Bazinska <julia@bazinski.com>
2023-05-29 12:26:12 +0200

    GAT tests separate for gpu and cpu

--- examples/gnn_benchmark/tests/tests_gat.py
+++ examples/gnn_benchmark/tests/tests_gat.py
@@ -61,6 +61,7 @@ def test_gat(bias, implementation, N, F, heads, seed):
     # Create input.
     graph = scipy.sparse.random(N, N, density=0.5, format='csr') + scipy.sparse.eye(N, format='csr')
     adj_matrix = SparseTensor.from_dense(torch.tensor(graph.A))
+    adj_matrix.set_value(None)
     print(adj_matrix)
     rowptr, col, _ = adj_matrix.csr()
     x = torch.rand((N, F_in))
@@ -82,30 +83,37 @@ def check_equal(expected_pred, pred):
         assert False
 
 
-# @pytest.mark.parametrize("N", [3, 7])
-# @pytest.mark.parametrize("F", [4, 9])
-# @pytest.mark.parametrize("heads", [1])
-# @pytest.mark.parametrize("seed", [2137, 402])
-@pytest.mark.parametrize("N", [3])
-@pytest.mark.parametrize("F", [4])
+@pytest.mark.parametrize("N", [3, 7])
+@pytest.mark.parametrize("F", [4, 9])
 @pytest.mark.parametrize("heads", [1])
-@pytest.mark.parametrize("seed", [2137])
+@pytest.mark.parametrize("seed", [2137, 402])
+# @pytest.mark.parametrize("N", [3])
+# @pytest.mark.parametrize("F", [4])
+# @pytest.mark.parametrize("heads", [1])
+# @pytest.mark.parametrize("seed", [2137])
 def test_spmm_single_head(N, F, heads, seed):
     if torch.cuda.is_available():
         import cupy as np
+        def ref(rowptrs, columns, features, e, output):
+            output[:] = 0
+            for l in range(N):
+                for v in range(np.asnumpy(rowptrs[l]), np.asnumpy(rowptrs[l + 1])):
+                    colv = columns[v]
+                    if heads == 1:
+                        for k in dace.map[0:F]:
+                            output[colv, k] += e[v, 0] * features[l, 0, k]
     else:
         import numpy as np
+        def ref(rowptrs, columns, features, e, output):
+            output[:] = 0
+            for l in range(N):
+                for v in range(rowptrs[l], rowptrs[l + 1]):
+                    colv = columns[v]
+                    if heads == 1:
+                        for k in dace.map[0:F]:
+                            output[colv, k] += e[v, 0] * features[l, 0, k]
     torch.random.manual_seed(42)
     np.random.seed(seed)
-
-    def ref(rowptrs, columns, features, e, output):
-        output[:] = 0
-        for l in range(N):
-            for v in range(rowptrs[l], rowptrs[l + 1]):
-                colv = columns[v]
-                if heads == 1:
-                    for k in dace.map[0:F]:
-                        output[colv, k] += e[v, 0] * features[l, 0, k]
 
     @dace.program
     def csrmm_prog(rowptrs, columns, features, e, output):
@@ -145,15 +153,7 @@ def test_spmm_single_head(N, F, heads, seed):
 # @pytest.mark.parametrize("N,F", [(3, 4)])
 # @pytest.mark.parametrize("heads", [2])
 # @pytest.mark.parametrize("seed", [2137])
-def test_spmm_many_heads(N, F, heads, seed):
-    if torch.cuda.is_available():
-        import cupy as np
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    print(device)
-    torch.random.manual_seed(42)
-    np.random.seed(seed)
-    dtype = np.float32
-
+def test_spmm_many_heads_cpu(N, F, heads, seed):
     def ref(rowptrs, columns, features, e, output):
         output[:] = 0
         for l in dace.map[0:N]:
@@ -163,6 +163,12 @@ def test_spmm_many_heads(N, F, heads, seed):
                     np.reshape(e[v], (heads, 1)) * features[l],
                     (heads * F,))
 
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    print(device)
+    torch.random.manual_seed(42)
+    np.random.seed(seed)
+    dtype = np.float32
+
     @dace.program
     def csrmm_prog(rowptrs, columns, features, e, output):
         output_perm = np.zeros((heads, N, F), dtype=dace.float32)  # H x N x F'
@@ -170,16 +176,21 @@ def test_spmm_many_heads(N, F, heads, seed):
         e_perm = np.transpose(e, (1, 0))  # H x num_entries
         # for h in dace.map[0:heads]:
         for h in range(heads):
-            output[h] = h
-            # csrmm(rowptrs, columns, e_perm[h], features_perm[h], output_perm[h], transA=True,
-            #       beta=1.0)
+            csrmm(rowptrs, columns, e_perm[h], features_perm[h], output_perm[h], transA=True,
+                  beta=1.0)
 
         output[:] = np.reshape(np.transpose(output_perm, (1, 0, 2)), (N, heads * F))
 
     graph = scipy.sparse.random(N, N, density=0.5, format='csr')
-    adj_matrix = SparseTensor.from_dense(torch.tensor(graph.A).to(device))
+    adj_matrix = SparseTensor.from_dense(torch.tensor(graph.A))
     print(adj_matrix)
     rowptr, col, _ = adj_matrix.csr()
+    if torch.cuda.is_available():
+        rowptr = np.array(rowptr.numpy(), copy=True)
+        col = np.array(col.numpy(), copy=True)
+    else:
+        rowptr = np.copy(rowptr.numpy())
+        col = np.copy(col.numpy())
     x = np.random.rand(N, heads, F).astype(dtype)
     vals = np.random.rand(col.shape[0], heads)
 
@@ -193,3 +204,80 @@ def test_spmm_many_heads(N, F, heads, seed):
     result = np.zeros((N, heads * F), dtype=dtype)
     csrmm_prog(rowptr, col, x, vals, result)
     check_equal(expected, result)
+
+
+# @pytest.mark.parametrize("N,F", [(3, 4)])
+# @pytest.mark.parametrize("heads", [2])
+# @pytest.mark.parametrize("seed", [2137])
+@pytest.mark.parametrize("N,F", [(3, 4), (7, 9)])
+@pytest.mark.parametrize("heads", [2, 3])
+@pytest.mark.parametrize("seed", [2137, 402])
+@pytest.mark.gpu
+def test_spmm_many_heads_gpu(N, F, heads, seed):
+    assert torch.cuda.is_available()
+    import cupy as cp
+
+    def ref(rowptrs, columns, features, e, output):
+        output[:] = 0
+        for l in dace.map[0:N]:
+            for v in range(cp.asnumpy(rowptrs[l]), cp.asnumpy(rowptrs[l + 1])):
+                colv = columns[v]
+                output[colv] += cp.reshape(
+                    cp.reshape(e[v], (heads, 1)) * features[l],
+                    (heads * F,))
+
+    device = torch.device('cuda')
+    torch.random.manual_seed(42)
+    np.random.seed(seed)
+    dtype = np.float32
+
+    @dace.program
+    def csrmm_prog(rowptrs, columns, features, e, output):
+        output_perm = np.zeros((heads, N, F), dtype=dace.float32)  # H x N x F'
+        # output_perm = np.reshape(np.zeros_like(output), (heads, N, F))  # H x N x F'
+        features_perm = np.transpose(features, (1, 0, 2))  # H x N x F'
+        e_perm = np.transpose(e, (1, 0))  # H x num_entries
+        # for h in dace.map[0:heads]:
+        for h in range(heads):
+            # e_tmp = np.zeros_like(e_perm[h])
+            # e_tmp[:] = e_perm[h]
+            # features_tmp = np.zeros_like(features_perm[h])
+            # features_tmp[:] = features_perm[h]
+            # output_tmp = np.zeros_like(output_perm[h])
+            csrmm(rowptrs, columns, e_perm[h], features_perm[h], output_perm[h], transA=True,
+                  beta=0.0)
+            # output_perm[h, :, :] = np.copy(output_tmp)
+
+            # e_tmp = np.zeros_like(e_perm[h])
+            # e_tmp[:] = e_perm[h]
+            # features_tmp = np.zeros_like(features_perm[h])
+            # features_tmp[:] = features_perm[h]
+            # output_tmp = np.zeros_like(output_perm[h])
+            # csrmm(rowptrs, columns, e_tmp, features_tmp, output_tmp, transA=True,
+            #       beta=0.0)
+            # output_perm[h, :, :] = np.copy(output_tmp)
+
+        output[:] = np.copy(np.reshape(np.transpose(output_perm, (1, 0, 2)), (N, heads * F)))
+
+    graph = scipy.sparse.random(N, N, density=0.5, format='csr')
+    adj_matrix = SparseTensor.from_dense(torch.tensor(graph.A))
+    print(adj_matrix)
+    rowptr, col, _ = adj_matrix.csr()
+    rowptr = cp.array(rowptr.numpy(), copy=True)
+    col = cp.array(col.numpy(), copy=True)
+    x = cp.random.rand(N, heads, F).astype(dtype)
+    vals = cp.random.rand(col.shape[0], heads).astype(dtype)
+
+    expected = cp.zeros((N, heads * F), dtype=dtype)
+    ref(rowptr, col, x, vals, expected)
+
+    result = cp.zeros((N, heads * F), dtype=dtype)
+    sdfg = csrmm_prog.to_sdfg(rowptr, col, x, vals, result)
+    sdfg.apply_gpu_transformations()
+    sdfg(rowptr, col, x, vals, result)
+    check_equal(expected, result)
+
+
+
+if __name__ == '__main__':
+    test_spmm_many_heads_gpu(3, 4, 2, 2137)
