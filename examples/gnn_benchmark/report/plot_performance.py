390156cbb2ef2a9b93b560e65d6c3197634bc866
Julia Bazinska <julia@bazinski.com>
2023-06-16 12:57:57 +0200

    Plot performance properly

--- examples/gnn_benchmark/report/plot_performance.py
+++ examples/gnn_benchmark/report/plot_performance.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 
 from examples.gnn_benchmark.report.plot import get_colors
 from examples.gnn_benchmark.report.plot_common import read_many_dfs, \
-    PLOT_FOLDER, DEFAULT_LABEL_MAP
+    PLOT_FOLDER, DEFAULT_LABEL_MAP, MODELING_FOLDER
 
 fwd_filenames = ['gcn-numbers.csv']
 bwd_filenames = ['gcn-numbers-bwd.csv']
@@ -14,35 +14,46 @@ def get_numbers(df: pd.DataFrame, dataset: str, backward: bool = False):
     filenames = bwd_filenames if backward else fwd_filenames
     df_list = []
     for filename in filenames:
-        df_list.append(pd.read_csv(filename))
+        df_list.append(pd.read_csv(MODELING_FOLDER / filename))
     numbers_df = pd.concat(df_list)
     numbers_df = numbers_df[numbers_df['Dataset'] == dataset]
 
-    df = df[df['Name'].str.contains('dace')]
-    df['Impl'] = df['Name'].str.replace('dace_', '')
+    # df = df[df['Name'].str.contains('dace')]
+    impls = df['Name'].str.replace('dace_', '')
+    impls = impls.str.replace('torch_', '')
+    impls = impls.str.replace('_compiled', '')
+    impls = impls.str.replace('edge_list', 'coo')
+    df['Impl'] = impls
+
     # Match entries from the numbers_df to the df based on Name, model, hidden
     # size.
     df = pd.merge(df, numbers_df, on=['Impl', 'Model', 'Size'], how='inner',
                   suffixes=('', '_numbers'))
-    df['Performance'] = df['Flops'] / df['Median'] * 1000  # (Time is in ms.)
-    df['Performance'] = df['Performance'] / 1e12  # TFLOP/s
+    perf = df['Flops'] * 1000 / df['Median']  # (Time is in ms.)
+    df['Performance'] = perf / 1e12  # TFLOP/s
     return df
 
 
 def make_performance_plot(full_df, name, dataset: str, backward: bool,
-                          labels=None, title=None):
+                          labels=None, title=None, drop_names=None):
     assert 'single' in name
-    df = get_numbers(full_df, dataset, backward=backward)
-    df = df.pivot(index='Size', columns='Name', values='Performance')
+    drop_names = drop_names or []
+    full_df = full_df[~full_df['Name'].isin(drop_names)]
+    full_df.reset_index(inplace=True)
 
+    df = get_numbers(full_df, dataset, backward=backward)
+    duplicated = df[df.duplicated(['Name', 'Model', 'Size'])]
+    if len(duplicated):
+        print(duplicated)
+    df = df.pivot(index='Size', columns='Name', values='Performance')
 
     colors = get_colors(df.columns)
     default_labels = DEFAULT_LABEL_MAP.copy()
     default_labels.update(labels or {})
     labels = default_labels
 
-    ax = df.plot(figsize=(5, 5), kind='barh', ylabel='Runtime [ms]',
-                       xlabel='Hidden size', color=colors)
+    ax = df.plot(figsize=(5, 8), kind='barh', ylabel='Runtime [ms]',
+                 xlabel='Hidden size', color=colors)
     ax.set_axisbelow(True)
     ax.yaxis.grid(color='lightgray', linestyle='--')
     ax.set_xlabel("Performance [TFLOP / s]")
@@ -73,19 +84,27 @@ def make_performance_plot(full_df, name, dataset: str, backward: bool,
 
 
 def plot_performance_gcn():
-    cora_filenames = ['15.06.11.23-gcn_single_layer-cora-203685.csv']
+    drop_names = ['torch_edge_list']
+
+    cora_filenames = [
+        '15.06.11.23-gcn_single_layer-cora-203685.csv',
+        '15.06.13.34-pyg-gcn_single_layer-cora-203692.csv',
+    ]
     cora_df, cora_bwd_df = read_many_dfs(cora_filenames, backward=True)
     make_performance_plot(cora_df, 'gcn-single-cora', dataset='cora',
-                          backward=False, title='Performance: GCN FWD, Cora')
+                          backward=False, drop_names=drop_names, title='Performance: GCN FWD, Cora')
     make_performance_plot(cora_bwd_df, 'gcn-single-cora', dataset='cora',
-                          backward=True, title='Performance: GCN BWD, Cora')
+                          backward=True, drop_names=drop_names, title='Performance: GCN BWD, Cora')
 
-    arxiv_filenames = ['15.06.12.59-gcn_single_layer-ogbn-arxiv-203691.csv']
+    arxiv_filenames = [
+        '15.06.12.59-gcn_single_layer-ogbn-arxiv-203691.csv',
+        '15.06.13.39-pyg-gcn_single_layer-ogbn-arxiv-203692.csv',
+    ]
     arxiv_df, arxiv_bwd_df = read_many_dfs(arxiv_filenames, backward=True)
     make_performance_plot(arxiv_df, 'gcn-single-arxiv', dataset='arxiv',
-                          backward=False, title='Performance: GCN FWD, OGB Arxiv')
+                          backward=False, drop_names=drop_names, title='Performance: GCN FWD, OGB Arxiv')
     make_performance_plot(arxiv_bwd_df, 'gcn-single-arxiv', dataset='arxiv',
-                          backward=True, title='Performance: GCN BWD, OGB Arxiv')
+                          backward=True, drop_names=drop_names, title='Performance: GCN BWD, OGB Arxiv')
 
 
 def main():
