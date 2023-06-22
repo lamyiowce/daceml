abc0d69f6de7c080b1d908572725900a8f3c7e0f
Julia Bazinska <julia@bazinski.com>
2023-06-22 12:03:40 +0200

    WIP: CSc for cSR gat bwd

--- examples/gnn_benchmark/tests/common.py
+++ examples/gnn_benchmark/tests/common.py
@@ -1,11 +1,13 @@
 import numpy as np
 
 
-def check_equal(expected_pred, pred, name=None, do_assert=True):
-    print('\n' + name if name else '')
-    print('Calculated: \n', pred)
-    print('Expected: \n', expected_pred)
-    if not np.allclose(pred, expected_pred, atol=1e-6):
+def check_equal(expected_pred, pred, name=None, do_assert=True, silent=False):
+    is_correct = np.allclose(pred, expected_pred, atol=1e-6)
+    if not silent or not is_correct:
+        print('\n' + name if name else '')
+        print('Calculated: \n', pred)
+        print('Expected: \n', expected_pred)
+    if not is_correct:
         max_err_abs = np.abs(pred - expected_pred).max()
         print("Abs error: ", max_err_abs)
         max_err_rel = max_err_abs / np.abs(expected_pred).max()
