236db4d6ff13a12b86d0615dfc71b1de48d27b44
Julia Bazinska <julia@bazinski.com>
2023-06-29 16:00:02 +0200

    GAT daceml GPU bwd WIP

--- examples/gnn_benchmark/implementations/gat_backward.py
+++ examples/gnn_benchmark/implementations/gat_backward.py
@@ -103,16 +103,22 @@ class GATConvBackwardCOO(BackwardImplementation):
                     col = columns[i]
                     dot_prods[col] += e[i] * d_alpha_vals[i]
 
+                dl = np.zeros((N,), dtype=val_dtype)
+                dr = np.zeros((N,), dtype=val_dtype)
                 dE_vals = np.zeros((num_entries,), dtype=val_dtype)
                 for i in dace.map[0:num_entries]:
                     col = columns[i]
                     mult = (d_alpha_vals[i] - dot_prods[col]) * e[i]
                     dE_vals[i] = mult
 
-                dC_vals = dE_vals * (C_vals > 0) + dE_vals * (C_vals <= 0) * negative_slope
+                dC_vals = np.empty((num_entries,), dtype=val_dtype)
+                for i in dace.map[0:num_entries]:
+                    dC_vals[i] = dE_vals[i] * (negative_slope + (1 - negative_slope) * C_vals[i] > 0)
+                    # dE_vals[i] * C_vals > 0 + dE_vals[i] * negative_slope * C_vals
 
-                dl = np.zeros((N,), dtype=val_dtype)
-                dr = np.zeros((N,), dtype=val_dtype)
+                    # dE_vals * (C_vals > 0) + dE_vals * (C_vals <= 0) * negative_slope
+
+
                 for i in dace.map[0:num_entries]:
                     col = columns[i]
                     dl[col] += dC_vals[i]
@@ -123,23 +129,25 @@ class GATConvBackwardCOO(BackwardImplementation):
 
                 dH_prime = np.zeros((N, F_out), dtype=val_dtype)
                 dH_prime[:] = 0
-                # dH_prime += dl[:, None] @ att_dst[0, 0, None, :] + dr[:, None] @ att_src[0, 0, None, :]
+                # # dH_prime += dl[:, None] @ att_dst[0, 0, None, :] + dr[:, None] @ att_src[0, 0, None, :]
 
                 coomm(A_rows=rows, A_cols=columns, A_vals=e, B=output_grad, C=dH_prime, beta=0.0,
                       transA=False)
 
-                for i, k in dace.map[0:N, 0:F_out] @ dace.dtypes.ScheduleType.Sequential:
+                for i, k in dace.map[0:N, 0:F_out]:
                     dH_prime[i, k] += dl[i] * att_dst[0, 0, k]
 
-                for i, k in dace.map[0:N, 0:F_out] @ dace.dtypes.ScheduleType.Sequential:
+                for i, k in dace.map[0:N, 0:F_out]:
                     dH_prime[i, k] += dr[i] * att_src[0, 0, k]
 
                 weight_grad = np.zeros((F_out, F_in), dtype=val_dtype)
-                weight_grad[:] = dH_prime.T @ node_features
+                weight_grad[:] = np.einsum('nf,nm->fm', dH_prime, node_features)
                 lin_srcDOTweight_grad[:] = weight_grad  # F_out x F_in
-                # d_x = dH_prime @ lin_srcDOTweight  # N x F_in
-                att_dst_grad[:] = features.T @ dl  # F_out
-                att_src_grad[:] = features.T @ dr  # F_out
+                # # d_x = dH_prime @ lin_srcDOTweight  # N x F_in
+                # att_dst_grad[:] = np.einsum('nf,n->f', features, dl)  # F_out
+                # att_src_grad[:] = np.einsum('nf,n->f', features, dr)  # F_out
+                att_dst_grad[:] = dl.T @ features  # F_out
+                att_src_grad[:] = dr.T @ features   # F_out
                 bias_grad[:] = np.sum(output_grad, axis=0)
         else:
             def backward_fn(node_features, rows, columns, lin_srcDOTweight,
@@ -245,7 +253,6 @@ class GATConvBackwardCOO(BackwardImplementation):
                 att_dst_grad[:] = features.T @ dl  # F_out
                 att_src_grad[:] = features.T @ dr  # F_out
                 bias_grad[:] = np.sum(output_grad, axis=0)
-
 
         result_node, result = autodiff_utils.backward_program_for_node(
             backward_fn, context, forward_node)
