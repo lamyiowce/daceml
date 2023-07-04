d6e7e959fb35030c02d1a578627782273cbde31e
Julia Bazinska <julia@bazinski.com>
2023-07-04 09:51:34 +0200

    GAT BWD multihead dace np

--- examples/gnn_benchmark/tests/tests_gat_bwd_multihead_dace.py
+++ examples/gnn_benchmark/tests/tests_gat_bwd_multihead_dace.py
@@ -10,6 +10,8 @@ from examples.gnn_benchmark.sparse_mm.coomm import coomm
 from examples.gnn_benchmark.tests.common import check_equal, check_grads, setup_data
 from examples.gnn_benchmark.tests.tests_mygat import MyGat
 
+torch.backends.cuda.matmul.allow_tf32 = False
+torch.backends.cudnn.allow_tf32 = False
 NEGATIVE_SLOPE = 0.2
 
 if torch.cuda.is_available():
@@ -24,13 +26,12 @@ def test_bwd_coo_dace():
     N = 3
     F_in = 2
     F_out = 4
-    heads = 2
+    heads = 1
     val_dtype = xp.float32
     negative_slope = 0.2
-    torch.random.manual_seed(42)
-    xp.random.seed(42)
+    torch.random.manual_seed(43)
+    xp.random.seed(43)
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
 
     _, layer, random_mask, x, graph = setup_data(N, F_in, F_out, heads)
     layer = layer.to(device)
@@ -49,14 +50,13 @@ def test_bwd_coo_dace():
         node_features: input features, N x M
         rows: rows, K
         columns: col, K
-        edge_vals: values, K
-        linDOTweight: F x M
-        bias: F
-        output: N x F
+        linDOTweight: H * F x M
+        bias: H * F
+        output: N x H * F
 
         node_features_grad: N x M
-        linDOTweight_grad: F x M
-        output_grad: N x F
+        linDOTweight_grad: H * F x M
+        output_grad: N x H * F
         """
 
         ### RECOMPUTE FORWARD VALUES ###
@@ -66,39 +66,37 @@ def test_bwd_coo_dace():
                                  lin_srcDOTweight)
         # features: N x H x F'
         features = np.reshape(features_tmp,
-                              (N, heads, num_out_features))
+                              (N, heads, F_out))
 
         # This ends up ridiculously slow because the outer loop is
         # executed on gpu and everything inside is executed
         # sequentially. The loop is moved to Sequential and the
         # inside matmul to GPU in my_auto_optimize.py.
 
-        features_perm = np.empty((heads, N, num_out_features), dtype=dtype)
-        for j, i, k in dace.map[0:heads, 0:N, 0:num_out_features]:
+        features_perm = np.empty((heads, N, F_out), dtype=val_dtype)
+        for j, i, k in dace.map[0:heads, 0:N, 0:F_out]:
             features_perm[j, i, k] = features[i, j, k]
 
-        alpha_src = dace.define_local((heads, N,), dtype=dtype)
-        alpha_dst = dace.define_local((heads, N,), dtype=dtype)
+        alpha_src = dace.define_local((heads, N,), dtype=val_dtype)
+        alpha_dst = dace.define_local((heads, N,), dtype=val_dtype)
         for h in dace.map[0:heads] @ dace.dtypes.ScheduleType.Sequential:
             alpha_src[h] = features_perm[h] @ att_src[0, h]
             alpha_dst[h] = features_perm[h] @ att_dst[0, h]
 
         # Calculate attention weights.
-        e = np.empty((heads, num_entries), dtype=dtype)
-        softmax_sum = np.zeros((N, heads), dtype=dtype)
+        e = np.empty((heads, num_entries), dtype=val_dtype)
+        softmax_sum = np.zeros((N, heads), dtype=val_dtype)
+        C_vals = np.empty((num_entries, heads), dtype=val_dtype)
 
         for h, i in dace.map[0:heads, 0:num_entries]:
             row = rows[i]
             col = columns[i]
             e_tmp = alpha_src[h, row] + alpha_dst[h, col]
+            C_vals[i, h] = e_tmp
             # # LeakyReLU
             e_tmp = np.maximum(negative_slope * e_tmp, e_tmp)
             e_tmp = np.exp(e_tmp)
             e[h, i] = e_tmp
-
-            # TODO: This is a workaround. With no schedule type, the results are incorrect with autoopt on CPU.
-            # for i in dace.map[0:num_entries]@dace.dtypes.ScheduleType.Sequential:
-            # col = columns[i]
             softmax_sum[col, h] += e[h, i]
 
         # Softmax normalization.
@@ -106,29 +104,29 @@ def test_bwd_coo_dace():
             colj = columns[j]
             e[h, j] = e[h, j] / softmax_sum[colj, h]
 
-
-
         ### COMPUTE THE GRADIENTS ###
-        d_alpha_vals = np.zeros((num_entries,), dtype=val_dtype)
+        output_grad_heads = np.reshape(output_grad, (N, heads, F_out))
+
+        d_alpha_vals = np.zeros((num_entries, heads), dtype=val_dtype)
         for i in dace.map[0:num_entries]:
             col = columns[i]
             row = rows[i]
-            d_alpha_vals[i] = np.dot(output_grad[col], features[row])
+            d_alpha_vals[i] = np.einsum('hf,hf->h', output_grad_heads[col], features[row])
 
-        dot_prods = np.zeros((N,), dtype=val_dtype)
+        dot_prods = np.zeros((N, heads), dtype=val_dtype)
         for i in dace.map[0:num_entries]:
             col = columns[i]
-            dot_prods[col] += e[i] * d_alpha_vals[i]
+            dot_prods[col] += e[:, i] * d_alpha_vals[i]
 
-        dE_vals = np.zeros((num_entries,), dtype=val_dtype)
+        dE_vals = np.zeros((num_entries, heads), dtype=val_dtype)
         for i in dace.map[0:num_entries]:
             col = columns[i]
-            dE_vals[i] = (d_alpha_vals[i] - dot_prods[col]) * e[i]
+            dE_vals[i] = (d_alpha_vals[i] - dot_prods[col]) * e[:, i]
 
         dC_vals = dE_vals * (C_vals > 0) + dE_vals * (C_vals <= 0) * negative_slope
 
-        dl = np.zeros((N,), dtype=val_dtype)
-        dr = np.zeros((N,), dtype=val_dtype)
+        dl = np.zeros((N, heads), dtype=val_dtype)
+        dr = np.zeros((N, heads), dtype=val_dtype)
 
         # Generates an incorrect SDFG!!!!
         # for i in dace.map[0:num_entries]:
@@ -145,30 +143,35 @@ def test_bwd_coo_dace():
             row = rows[i]
             dr[row] += dC_vals[i]
 
-        dH_prime = np.zeros((N, F_out), dtype=val_dtype)
+        dH_prime_perm = np.zeros((heads, N, F_out), dtype=val_dtype)
 
-        # for i, k in dace.map[0:num_entries, 0:F_out] @ dace.dtypes.ScheduleType.Sequential:
-        #     col = columns[i]
-        #     mult = e[i] * output_grad[col, k]
-        #     row = rows[i]
-        #     dH_prime[row, k] += mult
-        coomm(A_rows=rows, A_cols=columns, A_vals=e, B=output_grad, C=dH_prime, beta=1.0,
-              transA=False)
+        output_grad_perm = np.transpose(output_grad_heads, (1, 0, 2))
+        for i in range(heads):
+            coomm(A_rows=rows, A_cols=columns, A_vals=e[i], B=output_grad_perm[i],
+                  C=dH_prime_perm[i],
+                  beta=1.0, transA=False)
 
-        for i, k in dace.map[0:N, 0:F_out] @ dace.dtypes.ScheduleType.Sequential:
-            dH_prime[i, k] += dl[i] * att_dst[0, 0, k]
+        dH_prime = np.zeros((N, heads, F_out), dtype=val_dtype)
+        for h, i, k in dace.map[0:heads, 0:N, 0:F_out] @ dace.dtypes.ScheduleType.Sequential:
+            dH_prime[i, h, k] = dH_prime_perm[h, i, k]
 
-        for i, k in dace.map[0:N, 0:F_out] @ dace.dtypes.ScheduleType.Sequential:
-            dH_prime[i, k] += dr[i] * att_src[0, 0, k]
+        for h, i, k in dace.map[0:heads, 0:N, 0:F_out] @ dace.dtypes.ScheduleType.Sequential:
+            dH_prime[i, h, k] += dl[i, h] * att_dst[0, h, k]
+
+        for h, i, k in dace.map[0:heads, 0:N, 0:F_out] @ dace.dtypes.ScheduleType.Sequential:
+            dH_prime[i, h, k] += dr[i, h] * att_src[0, h, k]
 
         att_weights_grad[:] = d_alpha_vals  # K
         H_prime_grad[:] = dH_prime  # N x F_out
-        lin_srcDOTweight_grad[:] = dH_prime.T @ node_features  # F_out x F_in
-        node_features_grad[:] = dH_prime @ lin_srcDOTweight  # N x F_in
-        att_dst_grad[:] = features.T @ dl  # F_out
-        att_src_grad[:] = features.T @ dr  # F_out
-        bias_grad[:] = np.sum(output_grad, axis=0)
-        att_weights_out[:] = e  # K
+
+        dWeights = x.T @ np.reshape(dH_prime, (N, heads * F_out))
+        lin_srcDOTweight_grad[:] = dWeights.T  # F_out x F_in
+        node_features_grad[:] = np.reshape(dH_prime,
+                                           (N, heads * F_out)) @ lin_srcDOTweight  # N x F_in
+        att_dst_grad[:] = np.einsum('nhf,nh->hf', features, dl)  # F_out
+        att_src_grad[:] = np.einsum('nhf,nh->hf', features, dr)  # F_out
+        bias_grad[:] = np.reshape(np.sum(output_grad, axis=0), (heads * F_out,))
+        att_weights_out[:] = e.T  # K
         H_prime_out[:] = features  # N x F_out
 
     mygat = MyGat(weight=layer.lin_src.weight, att_src=layer.att_src,
@@ -187,16 +190,16 @@ def test_bwd_coo_dace():
     params['H_prime'] = mygat.H_prime
     params['att_weights'] = mygat.att_weights
 
-    weight_grad = np.zeros((F_out, F_in), dtype=val_dtype)
-    bias_grad = np.zeros((F_out,), dtype=val_dtype)
-    att_src_grad = np.zeros((1, 1, F_out), dtype=val_dtype)
-    att_dst_grad = np.zeros((1, 1, F_out), dtype=val_dtype)
+    weight_grad = np.zeros((F_out * heads, F_in), dtype=val_dtype)
+    bias_grad = np.zeros((F_out * heads,), dtype=val_dtype)
+    att_src_grad = np.zeros((1, heads, F_out), dtype=val_dtype)
+    att_dst_grad = np.zeros((1, heads, F_out), dtype=val_dtype)
     x_grad = np.zeros((N, F_in), dtype=val_dtype)
-    H_prime_grad = np.zeros((N, F_out), dtype=val_dtype)
-    att_weights_grad = np.zeros((num_entries,), dtype=val_dtype)
+    H_prime_grad = np.zeros((N, heads, F_out), dtype=val_dtype)
+    att_weights_grad = np.zeros((num_entries, heads), dtype=val_dtype)
 
-    att_weights_out_vanilla = np.zeros((num_entries,), dtype=val_dtype)
-    H_prime_out_vanilla = np.zeros((N, F_out), dtype=val_dtype)
+    att_weights_out_vanilla = np.zeros((num_entries, heads), dtype=val_dtype)
+    H_prime_out_vanilla = np.zeros((N, heads, F_out), dtype=val_dtype)
 
     x_cpu = x.get() if device == 'cuda' else x
     rows_cpu = rows.get() if device == 'cuda' else rows
@@ -221,8 +224,8 @@ def test_bwd_coo_dace():
     vanilla_result['x'] = xp.copy(x_grad)
     vanilla_result['H_prime'] = xp.copy(H_prime_grad)
     print(att_weights_grad.shape, rows.shape, cols.shape)
-    vanilla_result['att_weights'] = xp.copy(
-        xps.coo_matrix((xp.array(att_weights_grad), (cols, rows)), shape=(N, N)).todense())
+    vanilla_result['att_weights'] = None  # xp.copy(
+    # xps.coo_matrix((xp.array(att_weights_grad), (cols, rows)), shape=(N, N)).todense())
     vanilla_result['weight'] = xp.copy(weight_grad)
     vanilla_result['bias'] = xp.copy(bias_grad)
     vanilla_result['att_src'] = xp.copy(att_src_grad)
@@ -230,24 +233,26 @@ def test_bwd_coo_dace():
 
     check_equal(expected_pred=np.array(mygat.H_prime.detach().cpu()), pred=H_prime_out_vanilla,
                 name='H_prime')
-    check_equal(expected_pred=xp.array(mygat.att_weights.detach()),
-                pred=xps.coo_matrix((xp.array(att_weights_out_vanilla), (cols, rows)),
-                                    shape=(N, N)).todense(),
-                name='att_weights')
+    for h in range(heads):
+        check_equal(expected_pred=xp.array(mygat.att_weights.detach()[..., h]),
+                    pred=xps.coo_matrix((xp.array(att_weights_out_vanilla[:, h]), (cols, rows)),
+                                        shape=(N, N)).todense(),
+                    name=f'att_weights_{h}')
 
     check_grads(expected_params=params, result=vanilla_result)
     print("VANILLA FN OK!")
 
-    weight_grad = xp.zeros((F_out, F_in), dtype=val_dtype)
-    bias_grad = xp.zeros((F_out,), dtype=val_dtype)
-    att_src_grad = xp.zeros((1, 1, F_out), dtype=val_dtype)
-    att_dst_grad = xp.zeros((1, 1, F_out), dtype=val_dtype)
-    x_grad = xp.zeros_like(x, dtype=val_dtype)
-    H_prime_grad = xp.zeros((N, F_out), dtype=val_dtype)
-    att_weights_grad = xp.zeros((num_entries,), dtype=val_dtype)
+    weight_grad = xp.zeros((F_out * heads, F_in), dtype=val_dtype)
+    bias_grad = xp.zeros((F_out * heads,), dtype=val_dtype)
+    att_src_grad = xp.zeros((1, heads, F_out), dtype=val_dtype)
+    att_dst_grad = xp.zeros((1, heads, F_out), dtype=val_dtype)
+    x_grad = xp.zeros((N, F_in), dtype=val_dtype)
+    H_prime_grad = xp.zeros((N, heads, F_out), dtype=val_dtype)
+    att_weights_grad = xp.zeros((num_entries, heads), dtype=val_dtype)
 
-    att_weights_out = xp.zeros((num_entries,), dtype=val_dtype)
-    H_prime_out = xp.zeros((N, F_out), dtype=val_dtype)
+    att_weights_out = xp.zeros((num_entries, heads), dtype=val_dtype)
+    H_prime_out = xp.zeros((N, heads, F_out), dtype=val_dtype)
+
     with torch.no_grad():
         sdfg = backward_fn.to_sdfg(node_features=x, rows=rows, columns=cols,
                                    lin_srcDOTweight=xp.array(layer.lin_src.weight.cpu()),
