f1c66b03a30e98560b4894a8afee91f83476f780
Julia Bazinska <julia@bazinski.com>
2023-07-10 18:05:42 +0200

    count op int for spmms

--- examples/gnn_benchmark/report/modeling/flops.py
+++ examples/gnn_benchmark/report/modeling/flops.py
@@ -3,6 +3,7 @@ import dataclasses
 import dace
 import pandas as pd
 
+from examples.gnn_benchmark.report.modeling import measurable_ops
 from examples.gnn_benchmark.report.modeling.measurable_gat import GATConvCSR, GATConvCOO
 from examples.gnn_benchmark.report.modeling.measurable_layers import GCNConvCSR, \
     GCNConvCSRAdapt, BackwardGCNConvCSR, BackwardGCNConvCSRAdapt, GCNConvCOO, GCNConvCOOAdapt, \
@@ -37,8 +38,13 @@ def compute_all(layer_name, hidden_sizes, datasets, layers, filename,
                                        num_entries=dataset.num_edges,
                                        **layer_kwargs)
                 print(f"{dataset.name}, {hidden_size}: {layer}")
+                if hasattr('layer', 'impl_name'):
+                    op_name = layer.impl_name
+                else:
+                    op_name = layer.__class__.__name__
+
                 output += [
-                    (layer_name, layer.impl_name, dataset.name, hidden_size,
+                    (layer_name, op_name, dataset.name, hidden_size,
                      layer.flops(),
                      layer.flops() / 1024 ** 3,
                      layer.min_memory(),
@@ -52,12 +58,46 @@ def compute_all(layer_name, hidden_sizes, datasets, layers, filename,
     df.to_csv(filename, index=False)
 
 
+def compute_matmuls(layer_name, hidden_sizes, datasets, mm_ops, filename):
+    # model, impl_name, dataset, hidden_size, val_dtype, idx_dtype
+    output = []
+    for dataset in datasets:
+        for hidden_size in hidden_sizes:
+            for mm_op in mm_ops:
+                layer = mm_op(N=dataset.num_nodes,
+                              M=dataset.num_features,
+                              F=hidden_size,
+                              nnz=dataset.num_edges, val_dtype=dace.float32,
+                              idx_dtype=dace.int32)
+                print(f"{dataset.name}, {hidden_size}: {layer}")
+
+                op_name = layer.__class__.__name__
+
+                output += [
+                    (layer_name, op_name, dataset.name, hidden_size,
+                     layer.flops(),
+                     layer.flops() / 1024 ** 3,
+                     layer.min_memory(),
+                     layer.min_memory() / 1024 ** 3,
+                     layer.flops() / layer.min_memory())]
+
+    df = pd.DataFrame(output,
+                      columns=['Model', 'Op', 'Dataset', 'Size',
+                               'Flops', 'Gigaflops', 'Min memory bytes',
+                               'Min memory gb', 'Op intensity'])
+    df.to_csv(filename, index=False)
+
+
 def main():
     # Estimate the number of FLOPS and lower bound of memory movement for GAT
     # and GCN with typical sizes.
+    datasets = [CoraStats, ArxivStats]
+
+    matmuls = [measurable_ops.Csrmm, measurable_ops.Cscmm, measurable_ops.Coomm]
+    compute_matmuls('basic', [64], datasets,
+                    matmuls, 'basic-operators.csv')
 
     hidden_sizes = [8, 16, 32, 64, 128, 256, 512, 1024, 2048]
-    datasets = [CoraStats, ArxivStats]
     gcn_layers = [GCNConvCSR, GCNConvCSRAdapt, GCNConvCOO, GCNConvCOOAdapt, GCNConvCSC,
                   GCNConvCSCAdapt]
     backward_gcn_layers = [BackwardGCNConvCSR, BackwardGCNConvCSRAdapt, BackwardGCNConvCOO,
