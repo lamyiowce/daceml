f905a0e041969bcaaf31a330afc2fe92b4d9f515
jbazinska <julia.bazinska@gmail.com>
2023-07-25 18:12:37 +0200

    analyze dataset

--- examples/gnn_benchmark/analyze_datasets.py
+++ examples/gnn_benchmark/analyze_datasets.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tabulate
 
+from examples.gnn_benchmark import sparse
 from examples.gnn_benchmark.datasets import get_dataset
 
 
@@ -72,5 +73,74 @@ def compute_degrees(dataset):
     return degree
 
 
+def check_coalescence(data, name):
+    print(name)
+    # Convert to CSR.
+    # csr_data = sparse.CsrGraph.from_pyg_data(data, compute_input_grad=False)
+    #
+    # total_hits = 0
+    # cache_line_size = 32
+    # col_div = csr_data.columns // cache_line_size
+    # for i in range(len(csr_data.rowptrs) - 1):
+    #     values, counts = np.unique(col_div[csr_data.rowptrs[i]:csr_data.rowptrs[i + 1]], return_counts=True)
+    #     total_hits += np.sum(counts - 1)
+    #
+    # print(f'Cache hits horizontal: {total_hits}, {total_hits / csr_data.columns.shape[0] * 100:.2f}%')
+    #
+    # csc_data = sparse.CscGraph.from_pyg_data(data, compute_input_grad=False)
+    #
+    # total_hits = 0
+    # cache_line_size = 32
+    # col_div = csc_data.rows // cache_line_size
+    # for i in range(len(csc_data.colptrs) - 1):
+    #     values, counts = np.unique(col_div[csc_data.colptrs[i]:csc_data.colptrs[i + 1]], return_counts=True)
+    #     total_hits += np.sum(counts - 1)
+    #
+    # print(f'Cache hits vertical: {total_hits}, {total_hits / csc_data.rows.shape[0] * 100:.2f}%')
+
+    coo_data = sparse.CooGraph.from_pyg_data(data, compute_input_grad=False)
+
+    # Check if sorted.
+    last_row = -1
+    last_col = -1
+    sorted = True
+    for i in range(coo_data.rows.shape[0]):
+        col = coo_data.cols[i]
+        row = coo_data.rows[i]
+        if row < last_row:
+            sorted = False
+            break
+        else:
+            if row == last_row and col < last_col:
+                sorted = False
+                break
+        last_row = row
+        last_col = col
+    print("Sorted" if sorted else "Not sorted")
+
+    # Check if sorted.
+    last_row = -1
+    last_col = -1
+    sorted = True
+    for i in range(coo_data.rows.shape[0]):
+        col = coo_data.cols[i]
+        row = coo_data.rows[i]
+        if col < last_col:
+            sorted = False
+            break
+        if col == last_col and row < last_row:
+            sorted = False
+            break
+        last_row = row
+        last_col = col
+    print("Sorted" if sorted else "Not sorted")
+
+
 if __name__ == '__main__':
-    visualize_datasets()
+    # visualize_datasets()
+    check_coalescence(get_dataset('cora', 'cpu'), 'cora')
+    check_coalescence(get_dataset('ogbn-arxiv', 'cpu'), 'ogbn-arxiv')
+    check_coalescence(get_dataset('citeseer', 'cpu'), 'citeseer')
+    check_coalescence(get_dataset('pubmed', 'cpu'), 'pubmed')
+    check_coalescence(get_dataset('flickr', 'cpu'), 'flickr')
+    check_coalescence(get_dataset('reddit', 'cpu'), 'reddit')
