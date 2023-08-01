5cba21338d7fc7cfad736f78197fbc12507ab344
Julia Bazinska <julia@bazinski.com>
2023-08-01 16:47:15 +0200

    Fix memory order lol

--- examples/gnn_benchmark/check_memory.py
+++ examples/gnn_benchmark/check_memory.py
@@ -8,8 +8,8 @@ from examples.gnn_benchmark.experiment_info import ExperimentInfo
 from examples.gnn_benchmark.sdfg_util import get_total_memory, pretty_print_bytes
 
 
-def check_memory(dace_experiments: Dict[str, ExperimentInfo], outfile: Path = None):
-    headers = ['Model', 'Forward', 'Forward with grads', 'Backward']
+def check_memory(dace_experiments: Dict[str, ExperimentInfo], model_name, hidden_size, outfile: Path = None):
+    headers = ['Name', 'Model', 'Size', 'Forward', 'Forward with grads', 'Backward']
     rows = []
     for name, exp_info in dace_experiments.items():
         model_fwd = exp_info.model_eval
@@ -19,14 +19,15 @@ def check_memory(dace_experiments: Dict[str, ExperimentInfo], outfile: Path = No
             model_bwd.forward_sdfg) if model_bwd is not None else (0, 0)
         bwd_mem, _ = get_total_memory(
             model_bwd.backward_sdfg) if model_bwd is not None else (0, 0)
-        rows.append([name, fwd_mem, fwd_with_grad_mem, bwd_mem])
+        rows.append([name, model_name, hidden_size, fwd_mem, fwd_with_grad_mem, bwd_mem])
 
     print("MEMORY USAGE")
     print(tabulate.tabulate(
-        [[name] + [pretty_print_bytes(b) for b in vals] for name, *vals in rows],
+        [[vals[0]] + [pretty_print_bytes(b) for b in vals[-3:]] for vals in rows],
         headers=headers,
         tablefmt='github'))
 
     if outfile is not None:
         path = outfile.with_name(outfile.stem + "-memory.csv")
-        pd.DataFrame(rows, columns=headers).to_csv(path)
+        add_header = not path.exists()
+        pd.DataFrame(rows, columns=headers).to_csv(path, mode='a', header=add_header, index=False)
